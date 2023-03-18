2d90c8388bb9861a95ddae357a1cdb77c416410b
Thiago Anjos - TwPC <thiagowaltrick@gmail.com>
2023-03-18 20:54:11 -0300

    initianco consumo api

---
+++ Domain/InterfacesExternal/IPokemon.cs
@@ -0,0 +1,16 @@
+﻿using Entities.EntitiesExternal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.InterfacesExternal
+{
+    public interface IPokemon
+    {
+        List<Pokemon> List10PokemonRandom();
+        Pokemon GetPokemonById(int idPokemon);
+        Pokemon GetPokemonByName(string namePokemon);
+    }
+}
---
+++ Infrastructure/Repository/RepositoryExternal/RepositoryPokemon.cs
@@ -0,0 +1,34 @@
+﻿using Domain.InterfacesExternal;
+using Entities.EntitiesExternal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.RepositoryExternal
+{
+    public class RepositoryPokemon : IPokemon
+    {
+        //https://pokeapi.co/api/v2/pokemon/1
+        private readonly string urlApi = "https://pokeapi.co/api/v2/pokemon/";
+
+        public Pokemon GetPokemonById(int idPokemon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pokemon GetPokemonByName(string namePokemon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pokemon> List10PokemonRandom()
+        {
+            throw new NotImplementedException();
+        }
+
+        //creates...
+        //https://youtu.be/wxUk5mVGL7Y?list=PLP8qOphXwRnIOzXzoviI5xwxcc4-dV_pd&t=815
+    }
+}
